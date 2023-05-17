cd938719e4e1e3ae972198c92851e3f1425467c3
PhuBuii <20520273@gm.uit.edu.vn>
2023-05-17 13:24:58 +0700

    create show_vechicle.js

---
+++ asset/javascript/show_vechicle.js
@@ -0,0 +1,65 @@
+import * as THREE from "../node_modules/three/build/three.module.js";
+import { Car } from "./car.js";
+import { Truck } from "./truck.js";
+
+const scene = new THREE.Scene();
+
+const distance = 500;
+const camera = new THREE.OrthographicCamera(
+  window.innerWidth / -2,
+  window.innerWidth / 2,
+  window.innerHeight / 2,
+  window.innerHeight / -2,
+  0.1,
+  10000
+);
+
+camera.rotation.x = (50 * Math.PI) / 180;
+camera.rotation.y = (20 * Math.PI) / 180;
+camera.rotation.z = (10 * Math.PI) / 180;
+
+const initialCameraPositionY = -Math.tan(camera.rotation.x) * distance;
+const initialCameraPositionX =
+  Math.tan(camera.rotation.y) *
+  Math.sqrt(distance ** 2 + initialCameraPositionY ** 2);
+camera.position.y = initialCameraPositionY;
+camera.position.x = initialCameraPositionX;
+camera.position.z = distance;
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+scene.add(hemiLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+scene.add(dirLight);
+
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+var d = 500;
+dirLight.shadow.camera.left = -d;
+dirLight.shadow.camera.right = d;
+dirLight.shadow.camera.top = d;
+dirLight.shadow.camera.bottom = -d;
+
+const backLight = new THREE.DirectionalLight(0x000000, 0.4);
+backLight.position.set(200, 200, 50);
+backLight.castShadow = true;
+scene.add(backLight);
+
+const vechicle = new Car();
+scene.add(vechicle);
+
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  vechicle.rotation.z += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
