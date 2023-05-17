27b8164bb9c133c9a196cf4fea53f26970f54575
PhuBuii <20520273@gm.uit.edu.vn>
2023-05-17 22:03:55 +0700

    adding car & truck in game

---
+++ asset/javascript/car.js
@@ -0,0 +1,75 @@
+import * as THREE from "../node_modules/three/build/three.module.js";
+import { Texture, vechicleColors, zoom } from "./modules.js";
+import { Wheel } from "./wheel.js";
+
+const carFrontTexture = new Texture(40, 80, [{ x: 0, y: 10, w: 30, h: 60 }]);
+const carBackTexture = new Texture(40, 80, [{ x: 10, y: 10, w: 30, h: 60 }]);
+const carRightSideTexture = new Texture(110, 40, [
+  { x: 10, y: 0, w: 50, h: 30 },
+  { x: 70, y: 0, w: 30, h: 30 },
+]);
+const carLeftSideTexture = new Texture(110, 40, [
+  { x: 10, y: 10, w: 50, h: 30 },
+  { x: 70, y: 10, w: 30, h: 30 },
+]);
+
+export function Car() {
+  const car = new THREE.Group();
+  const color =
+    vechicleColors[Math.floor(Math.random() * vechicleColors.length)];
+
+  const main = new THREE.Mesh(
+    new THREE.BoxGeometry(60 * zoom, 30 * zoom, 15 * zoom),
+    new THREE.MeshPhongMaterial({ color, flatShading: true })
+  );
+  main.position.z = 12 * zoom;
+  main.castShadow = true;
+  main.receiveShadow = true;
+  car.add(main);
+
+  const cabin = new THREE.Mesh(
+    new THREE.BoxGeometry(33 * zoom, 24 * zoom, 12 * zoom),
+    [
+      new THREE.MeshPhongMaterial({
+        color: 0xcccccc,
+        flatShading: true,
+        map: carBackTexture,
+      }),
+      new THREE.MeshPhongMaterial({
+        color: 0xcccccc,
+        flatShading: true,
+        map: carFrontTexture,
+      }),
+      new THREE.MeshPhongMaterial({
+        color: 0xcccccc,
+        flatShading: true,
+        map: carRightSideTexture,
+      }),
+      new THREE.MeshPhongMaterial({
+        color: 0xcccccc,
+        flatShading: true,
+        map: carLeftSideTexture,
+      }),
+      new THREE.MeshPhongMaterial({ color: 0xcccccc, flatShading: true }), // top
+      new THREE.MeshPhongMaterial({ color: 0xcccccc, flatShading: true }), // bottom
+    ]
+  );
+  cabin.position.x = 6 * zoom;
+  cabin.position.z = 25.5 * zoom;
+  cabin.castShadow = true;
+  cabin.receiveShadow = true;
+  car.add(cabin);
+
+  const frontWheel = new Wheel();
+  frontWheel.position.x = -18 * zoom;
+  car.add(frontWheel);
+
+  const backWheel = new Wheel();
+  backWheel.position.x = 18 * zoom;
+  car.add(backWheel);
+
+  car.castShadow = true;
+  car.receiveShadow = false;
+
+  return car;
+}
